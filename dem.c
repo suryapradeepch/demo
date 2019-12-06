684a844bdd2b70180fc248ee356a126fab2fe7c3
suryapradeepch <42816290+suryapradeepch@users.noreply.github.com>
2019-12-06 22:03:23 +0530

    Update dem.c

--- dem.c
+++ dem.c
@@ -1,9 +1,34 @@
-#include <stdio.h>
- 
+#include<stdio.h>
+#include<stdlib.h>
+
 int main()
 {
-  printf("Hello world\n");
-  printf("Shreeram commit");
- printf("sameer commit");
-  return 0;
+    int *p, i, n;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    p = (int*)calloc(n, sizeof(int));
+
+    if(p==NULL)
+    {
+        printf("Memory allocation failed");
+        exit(1); // exit the program
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter %d element: ", i);
+        scanf("%d", p+i);
+    }
+    printf("\nprinting array of %d integers\n\n", n);
+
+    // calculate sum
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", *(p+i));
+    }
+    // signal to operating system program ran fine
+    return 0;
 }
